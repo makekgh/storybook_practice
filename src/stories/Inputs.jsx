d451d09c2783b341a9b084d0ad62e472ee7eae76
kgh <kgh@makeforyou.kr>
2024-02-02 16:34:50 +0900

    input

--- src/stories/Inputs.jsx
+++ src/stories/Inputs.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import './inputs.css';
 
-export const Input = ({ labelRequired, labelDisabled, labelText, placeholder, value, readonly, disabled, helpText, helpTextType }) => (
-    <div className="textfield" style={{ width: 240, padding: 40 }}>
-        <label htmlFor="" className={`label ${labelRequired} ${labelDisabled} `}>
-            {labelText}
-        </label>
-        <input type="text" placeholder={placeholder} value={value} readOnly={readonly} disabled={disabled} />
-        <span class={`help-text ${helpTextType}`}>{helpText}</span>
-    </div>
-);
+export const Input = ({ inputStatus, labelRequired, labelDisabled, labelText, successIcon, placeholder, value, readonly, disabled, helpText }) => {
+    const requiredLabel = labelRequired ? 'required' : '';
+    const disabledLabel = labelDisabled ? 'disabled' : '';
+    const iconSuccess = successIcon ? 'success-icon' : '';
+
+    return (
+        <div className={`textfield ${inputStatus}`} style={{ width: 240, padding: 40 }}>
+            <label htmlFor="" className={`label ${requiredLabel} ${disabledLabel}`}>
+                {labelText}
+            </label>
+            <input type="text" className={`${inputStatus} ${iconSuccess}`} placeholder={placeholder} value={value} readOnly={readonly} disabled={disabled} />
+            <span className={`help-text ${inputStatus}`}>{helpText}</span>
+        </div>
+    );
+};
+
+Input.propTypes = {
+    inputStatus: PropTypes.oneOf(['default', 'error', 'success']),
+    labelRequired: PropTypes.bool,
+    labelDisabled: PropTypes.bool,
+    labelText: PropTypes.string,
+    successIcon: PropTypes.bool,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    readonly: PropTypes.bool,
+    disabled: PropTypes.bool,
+    helpText: PropTypes.string,
+};
 
 Input.defaultProps = {
     labelText: 'Default',
+    labelRequired: true,
     placeholder: 'placeholder',
     helpText: 'help text',
 };
--- src/stories/Inputs.stories.js
+++ src/stories/Inputs.stories.js
@@ -9,4 +9,49 @@ export default {
     },
 };
 
-export const Default = {};
+export const Default = {
+    args: {
+        labelRequired: false,
+    },
+};
+
+export const Readonly = {
+    args: {
+        labelText: 'readonly input',
+        readonly: true,
+        value: 'readonly',
+    },
+};
+
+export const Disabled = {
+    args: {
+        labelText: 'disabled input',
+        disabled: true,
+        value: 'disabled',
+    },
+};
+
+export const Error = {
+    args: {
+        labelText: 'error input',
+        inputStatus: 'error',
+        value: 'error',
+    },
+};
+
+export const Success = {
+    args: {
+        labelText: 'success input',
+        inputStatus: 'success',
+        value: 'seccess',
+    },
+};
+
+export const SuccessIcon = {
+    args: {
+        labelText: 'with success icon',
+        value: 'it would be read-only',
+        successIcon: true,
+        readonly: true,
+    },
+};
