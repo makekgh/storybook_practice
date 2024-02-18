e07b1b8b90047b2242bb1574bc92f1a52fd9b7f5
publish <kgh@makeforyou.kr>
2024-02-18 22:00:27 +0900

    radio

--- src/stories/Radiobutton.jsx
+++ src/stories/Radiobutton.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import './selection.css';
 
-export const RadioButton = () => {
+const radioList = [
+    {
+        title: 'radio item',
+    },
+    {
+        title: 'radio item',
+    },
+    {
+        title: 'radio item',
+    },
+];
+
+export const RadioButton = ({ checked }) => {
     return (
         <div style={{ display: 'flex', gap: 20, paddingTop: 40 }}>
-            <div className="radio">
-                <input type="radio" id="radio1" name="sample2" className="" />
-                <label htmlFor="radio1" className="radio__item radio__item--fill">
-                    radio item1
-                </label>
-            </div>
+            {radioList.map((item, idx) => (
+                <div className="radio">
+                    <input type="radio" id={`radio${idx}`} name="sample2" className="" />
+                    <label htmlFor={`radio${idx}`} className="radio__item radio__item--fill" checked={checked}>
+                        {`${item.title} ${idx + 1}`}
+                    </label>
+                </div>
+            ))}
 
-            <div className="radio">
+            {/* <div className="radio">
                 <input type="radio" id="radio2" name="sample2" className="" />
                 <label htmlFor="radio2" className="radio__item radio__item--fill">
                     radio item2
@@ -23,7 +37,7 @@ export const RadioButton = () => {
                 <label htmlFor="radio4" className="radio__item radio__item--fill">
                     radio item3
                 </label>
-            </div>
+            </div> */}
         </div>
     );
 };
